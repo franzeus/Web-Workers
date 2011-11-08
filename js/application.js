54df74bde21bf3378a73cf379481ff7382e19ecc
Francy <webarbeit@gmail.com>
2011-11-08 12:09:46 +0100

    Added paper.txt.

--- js/application.js
+++ js/application.js
@@ -1,15 +1,15 @@
 var startButton = document.getElementById('startButton');
-startButton.addEventListener('click', function(e) { 
-
+// Start calculation Button
+startButton.addEventListener('click', function(e) {
   var numberOfThreads = document.getElementById('threadNum').value;
   var numToCalc = document.getElementById('calcNum').value;
-  if(numberOfThreads > 10) {
-    alert("No more than 10 Threads!");
+  if(numberOfThreads > 12) {
+    alert("Limit of 10 threads!");
     return false;
   }
 
   if(Server.supported()) {
-    Server.init(Math.min(numberOfThreads, 10), numToCalc);
+    Server.init(Math.min(numberOfThreads, 12), numToCalc);
     Server.createWorkers();
   }
 });
